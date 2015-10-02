64d5e0be6fede7acdb1dc585f305ca98b02873fb
Steve S Gee Jr <steve.gee@tekcomms.com>
2015-10-02 10:58:29 -0500

    FEAT-1001 added +setMesage:String:String

--- src/main/java/com/tekcomms/devops/gitflow/App.java
+++ src/main/java/com/tekcomms/devops/gitflow/App.java
@@ -6,9 +6,9 @@ public class App {
 	
 	public App(){}
 	public App(String msg){message = msg;}	
+	public String setMessage(String msg){ message = msg; }
 	public void speak(){System.out.println(message);	}
 	public String getMessage(){ return message; }
-	public String setMessage(String msg){ message = msg; }
 	
 	public static void main(String[] args) {
 		App app = new App();
